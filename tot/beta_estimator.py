ff9c5e85a09c2aff02ccea8a5dd8e67db674ff12
micklethepickle <michel_ma@hotmail.ca>
2017-07-17 14:15:32 -0400

    added time estimator

---
+++ tot/beta_estimator.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+
+
+# def normalize_timestamp(timestamps):
+# 	normalized = []
+# 	start = min(timestamps)
+# 	end = max(timestamps)
+# 	time_interval = end - start
+
+# 	for t in timestamps:
+# 		normalized.append(np.divide(np.array(timestamps) - start, time_interval))
+
+# 	return normalized
+		
+
+def estimate_beta(timestamps_normalized):
+	mean = np.mean(timestamps_normalized)
+	variance = np.divide(len(np.array(timestamps_normalized) - 1), np.sum(np.square(np.array(timestamps_normalized) - mean)))
+
+	mean_times_one_minus_mean_over_variance = np.divide(mean*(1-mean), variance)
+	alpha = mean*(mean_times_one_minus_mean_over_variance - 1)
+	beta = (1-mean)*(mean_times_one_minus_mean_over_variance - 1)
+
+	return alpha, beta
